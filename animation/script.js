5e2f785a06e8f627a8d6d0997713fa4b0a36c927
Valentin <choval@orange.fr>
2022-06-10 16:56:42 +0200

    commit

---
+++ animation/script.js
@@ -0,0 +1,2 @@
+TweenMax.to('.sequence',5,{repeat:-1,backgroundPosition: "0 -46680px",ease:SteppedEase.config(120)});
+TweenMax.to('.sequence1',5,{repeat:-1,backgroundPosition: "0 -39864px",ease:SteppedEase.config(66)});
